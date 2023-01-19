3ad499ff43d7f45d152514911efb53f068a6fa50
Petr Louda <plouda@c1r10s2.42prague.com>
2023-01-19 12:37:06 +0100

    Add ft_itoa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: plouda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 12:58:39 by plouda            #+#    #+#             */
-/*   Updated: 2023/01/19 09:16:28 by plouda           ###   ########.fr       */
+/*   Updated: 2023/01/19 11:51:54 by plouda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+
 #endif
